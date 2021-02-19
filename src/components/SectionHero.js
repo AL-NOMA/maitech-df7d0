243427c47fcf172d42d3b529ad83278adc1833bd
Stackbit Code Editor <support@stackbit.com>
2021-02-19 05:37:22 +0000

    SectionHero.js: changed

--- src/components/SectionHero.js
+++ src/components/SectionHero.js
@@ -8,6 +8,25 @@ export default class SectionHero extends React.Component {
     render() {
         let section = _.get(this.props, 'section', null);
         return (
+            <section id={_.get(section, 'section_id', null)} className="block hero-block bg-accent outer">
+              <div className="inner">
+                <div className="grid">
+                  <div className="cell block-content">
+                    {_.get(section, 'title', null) && (
+                    <h2 className="block-title underline">{_.get(section, 'title', null)}</h2>
+                    )}
+                    <div className="block-copy">
+                      {markdownify(_.get(section, 'content', null))}
+                    </div>
+                    {_.get(section, 'actions', null) && (
+                    <div className="block-buttons">
+                      <CtaButtons {...this.props} actions={_.get(section, 'actions', null)} />
+                    </div>
+                    )}
+                  </div>
+                </div>
+              </div>
+            </section>
         );
     }
 }
